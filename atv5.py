90a448243a2c393aec550c2f2ef69d9338403b08
pedro-henrique107 <72760426+pedro-henrique107@users.noreply.github.com>
2023-09-10 20:13:21 -0300

    Add files via upload

---
+++ atv5.py
@@ -0,0 +1,14 @@
+nome = "Pedro Machado"
+disciplina = "Engenharia Econômica para Software"
+
+# Função para exibir a mensagem
+def exibir_mensagem(nome, disciplina):
+    """
+    Esta função recebe um nome e uma disciplina como parâmetros
+    e exibe uma mensagem formatada.
+    """
+    print(f"Olá, meu nome é {nome}.")
+    print(f"Estou estudando a disciplina de {disciplina}.")
+
+# Chamando a função para exibir a mensagem
+exibir_mensagem(nome, disciplina)
